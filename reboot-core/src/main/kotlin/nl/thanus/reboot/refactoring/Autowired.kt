aa106b37aefc9a7b0a0e8edfd360afa67d4f62b9
thanus <thanus@users.noreply.github.com>
2020-02-28 09:57:22 +0100

    Add constructor without Lombok

--- reboot-core/src/main/kotlin/nl/thanus/reboot/refactoring/Autowired.kt
+++ reboot-core/src/main/kotlin/nl/thanus/reboot/refactoring/Autowired.kt
@@ -1,10 +1,20 @@
 package nl.thanus.reboot.refactoring
 
 import com.github.javaparser.ast.CompilationUnit
+import com.github.javaparser.ast.Modifier
+import com.github.javaparser.ast.NodeList
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration
 import com.github.javaparser.ast.body.FieldDeclaration
+import com.github.javaparser.ast.body.Parameter
 import com.github.javaparser.ast.expr.AnnotationExpr
+import com.github.javaparser.ast.expr.AssignExpr
+import com.github.javaparser.ast.expr.FieldAccessExpr
 import com.github.javaparser.ast.expr.Name
+import com.github.javaparser.ast.expr.NameExpr
+import com.github.javaparser.ast.expr.SimpleName
+import com.github.javaparser.ast.expr.ThisExpr
+import com.github.javaparser.ast.stmt.BlockStmt
+import com.github.javaparser.ast.stmt.ExpressionStmt
 
 fun rewriteAutowiredFieldInjections(compilationUnit: CompilationUnit) {
     if (isTest(compilationUnit)) {
@@ -27,11 +37,34 @@ private fun addConstructor(compilationUnit: CompilationUnit) {
             .any { isAutowiredAnnotation(it) }
 
     if (hasAutowiredAnnotation) {
-        compilationUnit.findAll(ClassOrInterfaceDeclaration::class.java)
-                .forEach { it.addMarkerAnnotation("RequiredArgsConstructor") }
+        addConstructorToClassesWithAutowired(compilationUnit)
+//        compilationUnit.findAll(ClassOrInterfaceDeclaration::class.java)
+//                .forEach { it.addMarkerAnnotation("RequiredArgsConstructor") }
 
-        compilationUnit.addImport("lombok.RequiredArgsConstructor")
+//        compilationUnit.addImport("lombok.RequiredArgsConstructor")
     }
+}
+
+private fun addConstructorToClassesWithAutowired(compilationUnit: CompilationUnit) {
+    compilationUnit.findAll(ClassOrInterfaceDeclaration::class.java)
+            .forEach { classOrInterfaceDeclaration ->
+                val fields = classOrInterfaceDeclaration.fields.filter { containsAutowiredAnnotation(it) }
+
+                val parameters = fields.map { Parameter(it.elementType, it.variables.first().name) }
+                val statements = fields.map {
+                    ExpressionStmt(
+                            AssignExpr(
+                                    FieldAccessExpr(ThisExpr(), it.variables.first().name.identifier),
+                                    NameExpr(SimpleName(it.variables.first().name.identifier)),
+                                    AssignExpr.Operator.ASSIGN
+                            )
+                    )
+                }
+
+                val constructor = classOrInterfaceDeclaration.addConstructor(Modifier.Keyword.PUBLIC)
+                constructor.parameters = NodeList(parameters)
+                constructor.body = BlockStmt(NodeList(statements))
+            }
 }
 
 private fun hasConstructor(compilationUnit: CompilationUnit): Boolean {
